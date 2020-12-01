85ec27618ea2cfea143ae328eaacd1732bf6d57b
sasoara <b.sarah2@hotmail.com>
2020-12-01 16:17:07 +0100

    format file

--- html/utils.php
+++ html/utils.php
@@ -4,7 +4,7 @@ function showDataTag(String $mime_type, String $secure_filename, String $filenam
     if (strpos($mime_type, 'image/', 0) !== false) {
         echo "<img class='imgview' src='showImg.php?path=$secure_filename' alt='vorschaubild'>";
     } elseif (strpos($mime_type, 'video/', 0) !== false) {
-        echo "<video autoplay class='imgview' controls poster='imgs/video.png'> 
+        echo "<video autoplay class='imgview' controls poster='imgs/video.png'>
                 <source src='showImg.php?path=$secure_filename'>
                 Sorry, your browser doesn't support embedded videos.
             </video>
@@ -12,11 +12,10 @@ function showDataTag(String $mime_type, String $secure_filename, String $filenam
     } elseif (strpos($mime_type, 'application/pdf', 0) !== false) {
         echo "<embed class='embed-pdf-file' src='showImg.php?path=$secure_filename' frameboarder='0' scrolling='yes'>
         </embed>";
-    }elseif (strpos($mime_type, 'application/', 0) !== false) {
+    } elseif (strpos($mime_type, 'application/', 0) !== false) {
         $path = "./imgs/document.png";
         echo "<img class='imgview' src='showImg.php?path=$path' alt='vorschaubild'>";
-    }
-    else {
+    } else {
         $path = "./imgs/document.png";
         echo "<img class='imgview' src='showImg.php?path=$path' alt='vorschaubild'>";
     }
