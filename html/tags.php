fe54ab6a55e2d1132ed6558e8489d0b3cf84bb62
sasoara <b.sarah2@hotmail.com>
2020-12-04 10:43:08 +0100

    formats tags.php file

--- html/tags.php
+++ html/tags.php
@@ -24,27 +24,27 @@ $page_structure = require_once("page_structure.php");
             ?>
 
             <?php // tags
-                    $email = $_SESSION['email'];
+            $email = $_SESSION['email'];
 
-                    $sql = "SELECT DISTINCT * 
+            $sql = "SELECT DISTINCT *
 
                     from tags as t
-                            
+
                     LEFT JOIN posts_has_tags AS pt ON t.id = pt.tags_id
-                            
+
                     LEFT JOIN posts AS p ON pt.posts_id = p.id
 
                     LEFT JOIN users AS u ON p.users_id = u.id
 
                     WHERE p.is_public like 1 or u.email like ('$email');";
 
-                    # tags ordered by name ascending
-                    $tags = $dbh->query($sql);
+            # tags ordered by name ascending
+            $tags = $dbh->query($sql);
 
-                    # counter that counts all saved tags in db for different tag-col preview  
-                    $counting_tags = $tags->rowCount();
-                    $size_on_tags = 'tag-col-';
-                    $size_on_tags = $counting_tags <= 50 ? 'tag-col-small' : 'tag-col-large';
+            # counter that counts all saved tags in db for different tag-col preview
+            $counting_tags = $tags->rowCount();
+            $size_on_tags = 'tag-col-';
+            $size_on_tags = $counting_tags <= 50 ? 'tag-col-small' : 'tag-col-large';
             ?>
             <h1 class="flex setDown">Tags</h1>
             <div class="amount-of-tags">Amount of tags: <?= $counting_tags ?></div>
@@ -52,9 +52,9 @@ $page_structure = require_once("page_structure.php");
                 <section class="tag-col <?= $size_on_tags ?>">
                     <?php
                     foreach ($tags as $tag) {
-                        ?>
-                    <a href="search.php?term=%23<?= $tag[1] ?>">#<?= $tag[1] ?>
-                    </a>
+                    ?>
+                        <a href="search.php?term=%23<?= $tag[1] ?>">#<?= $tag[1] ?>
+                        </a>
                     <?php
                     } // %23 = # in UTF-8 (https://www.urlencoder.org/)
                     ?>
