35c6dfef1f48edb742f19754f33ac93474aa8dd3
sasoara <b.sarah2@hotmail.com>
2021-01-05 11:30:01 +0100

    corrects HTTP_REFERER query

--- html/snippets/session_handling.php
+++ html/snippets/session_handling.php
@@ -21,9 +21,8 @@ session_set_cookie_params(
 // Starting the session
 session_start();
 
-// TODO: Scheitert!! Pfad ist nicht korrekt (für index.php)!
 // Checks where the request come from
-if (basename($_SERVER['HTTP_REFERER'], '?' . $_SERVER['QUERY_STRING']) == 'upload_validation.php' || basename($_SERVER['HTTP_REFERER'], '?' . $_SERVER['QUERY_STRING']) == 'uploadForm_validation.php') {
+if (basename($_SERVER['HTTP_REFERER'], '?' . $_SERVER['QUERY_STRING']) == 'uploadForm.php' || basename($_SERVER['HTTP_REFERER'], '?' . $_SERVER['QUERY_STRING']) == 'upload.php') {
     $path_to_index = '../../index.php';
 } else {
     $path_to_index = '../index.php';
